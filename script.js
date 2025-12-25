dfd8a4ea841cc27948c11a252a0ef0fad5fc5824
Abel Yesfa <abelyesfa5@gmail.com>
2025-12-25 02:38:51 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/*============= typing animation ==========*/
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphics Designer","Youtuber","Computer Maintainer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
